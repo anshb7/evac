5e7c1dbab0a16873c5727fd27387e63f27d5bbe2
sanya-mahajan <88281057+sanya-mahajan@users.noreply.github.com>
2023-02-26 15:50:21 +0530

    display people connected

--- Web Socket Server/server.js
+++ Web Socket Server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const { createServer } = require("http");
 const mongoose = require('mongoose');
 const { Server } = require("socket.io");
@@ -6,11 +7,8 @@ const { initializeRoutes } = require("./routes/index");
 const  Outlet  = require('../REST Server/models/outletModel');
 const  User  = require('../REST Server/models/userModel');
 const dotenv = require('dotenv');
-const getAllOutlets = require('../REST Server/controllers/userController');
-
-for (let i=0;i<getAllOutlets.length;i++){
-  console.log(getAllOutlets[i]);
-}
+dotenv.config({ path: './config.env' });
+const { ObjectId } = require('mongodb');
 
 mongoose.set('strictQuery', true);
 let app = express();mongoose.set('strictQuery', true);
@@ -38,7 +36,9 @@ const io = new Server(httpServer, {
 httpServer.listen(port, () => {
   console.log(`evac app listening on port ${port}`);
 });
-
+const id='63faf45eff56af53379127c4';//id for the outlet of logged in admin
+let peoplePresent=[];
+let headCount=0;
 // Connect to MongoDB using Mongoose
 // mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect('mongodb+srv://adityaparmar:PVGtxnovwPjOqmaj@cluster-evac.xggkhjh.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
@@ -47,6 +47,64 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB using Mongoose');
+  
+
+  // Get the collection
+  const collection = db.collection('outlets');
+  const users=db.collection('users');
+
+  // Find all documents
+  collection.find({}).toArray(function(err, docs) {
+    console.log("Found the following records:");
+    for(let i=0;i<docs.length;i++){
+      
+      if(docs[i]._id==id){
+        console.log('found');
+        console.log(docs[i].headCount);headCount=docs[i].headCount;
+        
+        for(let j=0;j<docs[i].peoplePresent.length;j++){
+          peoplePresent.push(docs[i].peoplePresent[j].toString());
+        }
+        console.log(peoplePresent);
+        break;
+      }
+    }
+  });
+  users.find({}).toArray(function(err, docs) {
+    console.log("Found the following records:");
+    for(let i=0;i<peoplePresent.length;i++){
+
+      for(let j=0;j<docs.length;j++){
+        if(peoplePresent[i]==docs[j]._id.toString()){
+          console.log(docs[j].name);
+        }
+      }
+    }
+  });
+  // Set up Socket.io
+  
+io.on( "connection", socket => {
+  console.log( "New client connected" );
+  
+
+  peoplePresent.push(socket.id);
+   // get the count of currently connected users
+  const connectedUsers = io.sockets.sockets.size;
+  console.log(`Total checked in users: ${headCount+connectedUsers}`);
+  console.log(peoplePresent);
+  
+
+  // handle disconnect event
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+    // get the count of connected users
+    const connectedUsers = io.sockets.sockets.size;
+    console.log(`Total checked in users: ${headCount+connectedUsers}`);
+    peoplePresent.pop(socket.id);
+    console.log(peoplePresent);
+      
+  });
+  });
   
 // Set up the MongoDB change stream
 const changeStream = User.watch({ $match: { 'operationType': 'update' } });
@@ -66,81 +124,5 @@ changeStream.on('change', (change) => {
 
 
 
-// const id=  Outlet.findOne({},'_id', function(err, result) {
-//   if (err) throw err;
-//   console.log(result._id);
-//   return result._id;
-// });
-const id='63faf45eff56af53379127c4';
 
-io.on( "connection", socket => {
-  console.log( "New client connected" );
-
-  
-   // get the count of currently connected users
-  const connectedUsers = io.sockets.sockets.size;
-  console.log(`Total connected users: ${connectedUsers}`);
-  
-    Outlet.findById(id, (err, record) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-
-      console.log(record.name);
-      record.headCount = io.sockets.sockets.size;
-      //add person from list of people present in outlet
-      let addedUser= User.findById(io.sockets.sockets.id)
-      record.peoplePresent.push(addedUser);
-      io.emit(addedUser.location, "joined");
-       
-      record.save((err) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        console.log('headcount updated !');
-       
-      });
-    });
-
-  
-  
-
-  
-
-  // handle disconnect event
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-    // update the count of connected users
-    const connectedUsers = io.sockets.sockets.size;
-    console.log(`Total connected users: ${connectedUsers}`);
-      
-      Outlet.findById(id, (err, record) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        record.headCount = io.sockets.sockets.size;
-        //remove person from list of people present in outlet
-      let removedUser= User.findById(io.sockets.sockets.id)
-      record.peoplePresent.pull(removedUser);
-      io.emit(removedUser.location, "left");
-
-         
-        record.save((err) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          console.log('headcount updated !');
-        });
-      });
-    
-
-  });
-  
-
-
-  } );
 
